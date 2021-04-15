461edba9bb92bb0c9ebaa7fa415fa65f5bc4d42b
George Huliaris <georgehuliaris@me.com>
2021-04-14 21:40:57 -0500

    added to product.js, but think I forgot to save

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,14 +9,19 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
     type: DataTypes.INT,
+    autoIncrement: true,
     allowNull: false,
     primaryKey: true,
-    autoIncrement: true,
   },
     product: {
-    type: DataTypes.DECIMAL(8, 2),
+    type: DataTypes.STR,
     allowNull: false,
+    },
+    price: {
+      
+    }
   },
   {
     sequelize,
