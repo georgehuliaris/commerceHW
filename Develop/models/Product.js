341914368ed4efdb9b81cd5865a1773fd5c08cb8
George Huliaris <georgehuliaris@me.com>
2021-04-14 21:56:02 -0500

    added to product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,25 +10,45 @@ class Product extends Model {}
 Product.init(
   {
     id: {
-    type: DataTypes.INT,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
+      type: DataTypes.INT,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
     product: {
-    type: DataTypes.STR,
-    allowNull: false,
+      type: DataTypes.STR,
+      allowNull: false,
     },
     price: {
-      
-    }
+      type: DataTypes.DEC(8, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INT,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INT,
+      references: {
+        model: "category",
+        key: "id",
+        unique: false,
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
